64aceb25bcde64a74761dd93aac6d8da01257fe7
ThangDinh1814 <thangdinh1814@gmail.com>
2016-04-21 15:29:00 -0400

    Added files via upload

--- loadsave.cpp
+++ loadsave.cpp
@@ -1,36 +1,30 @@
-#include "dork.h'
+//#include "loadsave.h"
+#include "dork.h"
 #include "game.h"
 #include <fstream>
 #include <string>
 #include <iostream>
-
 void save(character &a){
-	ofstream outfile;
-	outfile.open("Save1.dat");
-	outfile << a.getName() << endl;
-	outfile << a.getLevel() << endl;
-	outfile << a.getEnergy() << endl;
-	outfile << a.getSteps() << endl;
-	outfile.close();
+ofstream outfile;
+outfile.open("Save1.dat");
+outfile << a.getName() << " " << a.getLevel() << " " << a.getEnergy() << a.getSteps();
+outfile.close();
 }
 
 void load(character &a){
-	ifstream myfile	
+	ifstream myfile;
 	myfile.open ("Save1.dat");
-	if (!myfile){
+		if (!myfile){
 		cerr<<"Could not open file" << endl;
-		return -1;
-	}
+		exit(1);
+			}
 	string n,l;
 	int e,s;
-	myfile >> n >> endl;
-	myfile >> l >> endl;
-	myfile >> e >> endl;
-	myfile >> s >> endl;
-		a.setName(n);
-		a.setLevel(l);
-		a.setEnergy(e);
-		a.setSteps(s);
-		
+	myfile >> n >> l >> e >> s;
+	a.setName(n);
+	a.setLevel(l);
+	a.setEnergy(e);
+	a.setSteps(s);
+
 	myfile.close();
 }
