64cf1202b330f4e774d5c1dfe1feab1932fa7e86
Debuggerapprentice <thebirdiegolfer@gmail.com>
2016-04-21 14:24:50 -0400

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -7,14 +7,42 @@ using namespace std;
 
 character :: character() : name("unknown"), level_name("terratorial"), energy(10), steps(0){
 }
+
 void character :: setName(string in_name){ 
 	name = in_name; 
 } 
-string character :: getName(){ 
+
+void setLevel(string in_level){
+	level_name=in_level;
+}
+
+void setEnergy(int in_energy){
+	energy=in_energy;
+}
+
+void setSteps(int in_steps){
+	steps=in_steps;
+} 
+
+string character::getName(){ 
 	return name; 
 } 
-void character :: printCharacter() { 
-	printw("------------------------------------------------------------------------------ \n");
-	printw("Hello %s   level: %s \t energy: %i   steps: %i \n", name.c_str(), level_name.c_str(), energy, steps);
-	printw("------------------------------------------------------------------------------ \n"); 
+
+string character::getLevel(){
+	return level_name;
 }
+
+int character::getEnergy(){
+	return energy;
+}
+
+int character::getSteps(){
+	return steps;
+}
+
+void character :: printCharacter() { 
+printw("------------------------------------------------------------------------------ \n");
+printw("Hello %s   level: %s \t energy: %i   steps: %i \n", name.c_str(), level_name.c_str(), energy, steps);
+printw("------------------------------------------------------------------------------ \n"); 
+}
+
