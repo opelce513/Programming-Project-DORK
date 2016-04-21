527245104a33b6be4958540a3e5736a69dc80591
ThangDinh1814 <thangdinh1814@gmail.com>
2016-04-21 15:24:46 -0400

    my version

--- game.cpp
+++ game.cpp
@@ -3,46 +3,37 @@
 #include <string> 
 #include <stdio.h> 
 
+
 using namespace std; 
 
 character :: character() : name("unknown"), level_name("terratorial"), energy(10), steps(0){
 }
-
 void character :: setName(string in_name){ 
 	name = in_name; 
 } 
-
-void setLevel(string in_level){
-	level_name=in_level;
-}
-
-void setEnergy(int in_energy){
-	energy=in_energy;
-}
-
-void setSteps(int in_steps){
-	steps=in_steps;
-} 
-
-string character::getName(){ 
+string character :: getName(){ 
 	return name; 
+}
+int  character :: getEnergy(){ 
+  return energy; 
 } 
-
-string character::getLevel(){
-	return level_name;
+int character :: getSteps(){ 
+  return steps; 
 }
-
-int character::getEnergy(){
-	return energy;
+void character :: setEnergy(int in_energy){ 
+  energy = in_energy; 
+} 
+void character :: setSteps(int in_steps){ 
+  steps = in_steps; 
+} 
+void character :: setLevel(string in_level){ 
+  level_name = in_level; 
+} 
+string character :: getLevel(){ 
+  return level_name; 
 }
-
-int character::getSteps(){
-	return steps;
-}
-
 void character :: printCharacter() { 
-printw("------------------------------------------------------------------------------ \n");
-printw("Hello %s   level: %s \t energy: %i   steps: %i \n", name.c_str(), level_name.c_str(), energy, steps);
-printw("------------------------------------------------------------------------------ \n"); 
+	printw("------------------------------------------------------------------------------ \n");
+	printw("Hello %s   level: %s \t energy: %i   steps: %i \n", name.c_str(), level_name.c_str(), energy, steps);
+	printw("------------------------------------------------------------------------------ \n"); 
 }
-
