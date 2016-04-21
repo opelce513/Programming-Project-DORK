4a578d394da514569a1e8cf68b2ba0270f692e1b
Debuggerapprentice <thebirdiegolfer@gmail.com>
2016-04-21 15:57:02 -0400

    Update loadsave.cpp

--- loadsave.cpp
+++ loadsave.cpp
@@ -1,4 +1,3 @@
-//#include "loadsave.h"
 #include "dork.h"
 #include "game.h"
 #include <fstream>
@@ -7,7 +6,11 @@
 void save(character &a){
 ofstream outfile;
 outfile.open("Save1.dat");
-outfile << a.getName() << " " << a.getLevel() << " " << a.getEnergy() << a.getSteps();
+outfile << a.getName() << endl;
+outfile << a.getLevel() << endl;
+outfile << a.getEnergy() << endl;
+outfile << a.getSteps() << endl;
+
 outfile.close();
 }
 
@@ -15,12 +18,39 @@ void load(character &a){
 	ifstream myfile;
 	myfile.open ("Save1.dat");
 		if (!myfile){
-		cerr<<"Could not open file" << endl;
-		exit(1);
+		printw("Could not open file \n");
+		printw("Please make a new character you be breaking my game bro. Not cool :( \n");
+		printw("What is your name?: \n");
+		char str[20];
+		getstr(str); 
+		a.setName(str);
+		a.getName();
+		a.printCharacter(); 
 			}
-	string n,l;
+
+	string n,l,line;
 	int e,s;
-	myfile >> n >> l >> e >> s;
+	int count=0;
+    while (getline(myfile, line)) {
+	if (count == 0){
+	myfile >> n;
+	count++;
+	}
+	else if (count == 1){
+	myfile >> l;
+	count++;
+	}
+	else if (count == 2){
+	myfile >> e;
+	count++;
+	}
+	else if (count == 3){
+	myfile >> s;
+	count++;
+	}
+	else {
+	}
+	}
 	a.setName(n);
 	a.setLevel(l);
 	a.setEnergy(e);
