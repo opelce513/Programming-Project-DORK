fcabf3b757eab2932e75f4a19d87bb2824689a2d
Debuggerapprentice <thebirdiegolfer@gmail.com>
2016-04-25 10:33:49 -0400

    Update checkpoint.cpp

--- checkpoint.cpp
+++ checkpoint.cpp
@@ -1,38 +1,18 @@
+#include "dork.h"
+#include "game.h"
+#include <fstream>
+#include <string>
 #include <iostream>
-
 using namespace std;
 
-Checkpoint()
-{
-for(i=0; i<200; i+=25){
-    if(i == 25 && i<=200)
-    {
-    }
-    else if (i == 50 && i<=200)
-    {
-    }
-    else if (i == 75 && i<=200)
-    {
-    }
-    else if (i == 100 && i<=200)
-    {
-    }
-    else if (i == 125 && i<=200)
-    {
-    }
-    else if (i == 150 && i<=200)
-    {
-    }
-    else if (i == 175 && i<=200)
-    {
-    }
-    else if (i == 200 && i<=200)
-    {
-    }
-    else(){
+void save(character& a);
+void checkpoint(int num, int roll,character& a){
+for(int i=25; i<200; i+=25){
+   if (((num+roll)>=i)&&((num<i))){
+	save(a);
+	}
+    else {
     printw("Something went wrong \n");
     }
-    
-    
-
-
+}
+}
